ca591766ea9f0fb41c7b01e1a1243e6e23e303b1
Lisias T <me@lisias.net>
2021-10-21 23:26:24 -0300

    Adding propper ModuleManager support

---
+++ Source/KramaxAutoPilot/ModuleManagerSupport.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Kramax
+{
+	public static class ModuleManagerSupport
+	{
+		public static IEnumerable<string> ModuleManagerAddToModList()
+		{
+			string[] r = {Version.Namespace};
+			return r;
+		}
+	}
+}
